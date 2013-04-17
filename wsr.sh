45a2f45bf0575b39579b9e184d860ab2f891dcd5
Alexander Sedov <elec.lomy.ru@gmail.com>
2013-04-17 17:26:35 +0400

    Typo fixes.

--- wsr.sh
+++ wsr.sh
@@ -1,14 +1,19 @@
 #!sh
 
 _wsr_session=`mktemp -td "wsr.$$.XXXXXXXX"`
+trap _wsr_cleanup EXIT
 _wsr_counter=0
 
+_wsr_cleanup () {
+	rm -r "$_wsr_session"
+}
+
 wsr_load_session() {
-    cp "$1"/*.data $_wsr_session/
+    cp "$1"/*.data "$_wsr_session/"
 }
 
 wsr_save_session() {
-    cp $_wsr_session/*.data "$1"/
+    cp "$_wsr_session/"*.data "$1"/
 }
 
 wsr_back() {
@@ -69,27 +74,27 @@ _wsr_request() {
 	command="curl `_wsr_curl_options $2` -w %{http_code}\\\n%{url_effective}\\\\n%{content_type} \"$1\""
     extra_info=`eval "$command"`
     if [ $? -ne 0 ]; then
-        rm $filename.*
+        rm "$filename.*"
         return 255
     fi
     http_code=`echo "$extra_info"|head -n1`
-    effective_url=`echo "$extra_info"|tail -n+2|head -n1|tee $filename.url`
+    effective_url=`echo "$extra_info"|tail -n+2|head -n1|tee "$filename.url"`
     content_type=`echo "$extra_info"|tail -n+3|head -n1`
     encoding=`cat $2.enc 2>/dev/null`
-    encoding=`_wsr_get_encoding $filename "$content_type" "$encoding"|head -n1|tee $filename.enc`
+    encoding=`_wsr_get_encoding "$filename" "$content_type" "$encoding"|head -n1|tee "$filename.enc"`
     if [ "$encoding" != "x-user-defined" ]; then
-        iconv -f "$encoding" -t 'utf-8' $filename.body -o $filename.body
+        iconv -f "$encoding" -t 'utf-8' "$filename.body" -o "$filename.body"
     fi
 }
 
-_wsr_enc_bom_utf16le=`printf \\xef\\xff`
-_wsr_enc_bom_utf16be=`printf \\xff\\xef`
-_wsr_enc_bom_utf8=`printf \\xef\\xbb\\xbf`
+_wsr_enc_bom_utf16le=`printf "\\xef\\xff"`
+_wsr_enc_bom_utf16be=`printf "\\xff\\xef"`
+_wsr_enc_bom_utf8=`printf "\\xef\\xbb\\xbf"`
 
 _wsr_detect_encoding() {
     local headerenc bodyenc
     # BOM
-    case `head -c3 $1.body` in
+    case `head -c3 "$1.body"` in
         $_wsr_enc_bom_utf16le*) echo utf-16le; return 0;
             ;;
         $_wsr_enc_bom_utf16be*) echo utf-16be; return 0;
@@ -103,9 +108,11 @@ _wsr_detect_encoding() {
         echo "$headerenc"
         return 0
     fi
-    cat $2.body | html-encoding-prescan
+	if which html-encoding-prescan >/dev/null; then
+		html-encoding-prescan "$1.body"
+	fi
     if which enca >/dev/null; then
-        enca -i $1.body
+        enca -i "$1.body"
     fi
     if which chardet >/dev/null; then
         chardet $1.body | awk '{print $2}'
@@ -115,12 +122,12 @@ _wsr_detect_encoding() {
 _wsr_get_encoding() {
     _wsr_detect_encoding "$1" "$2" | # tr -cd "[:alnum:]-" |
 	while read x; do
-        tabled=`_wsr_encoding_table | awk "/^$x /"' { print $2; }'`
+		tabled=`_wsr_encoding_table | awk ' { if ($2 == '"\"$x\""') print $1; }'`
 		if [ -z "$tabled" ]; then echo "$x"; else echo "$tabled"; fi
     done |
     while read x; do
-        if [ "$x" == "x-user-defined" ]; then
-            print "$x";
+        if [ "$x" = "x-user-defined" ]; then
+            echo "$x"
         elif iconv -f "$x//IGNORE" -t "$x//IGNORE" >/dev/null </dev/null 2>/dev/null; then
             echo "$x"
         fi
